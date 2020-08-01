7899d962f817ed2772be69845980ba8f0f581bd4
ThunderGunExpress <32972732+ThunderGunExpress@users.noreply.github.com>
2020-08-01 05:08:26 -0700

    Init

---
+++ BringYourOwnBuilder/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Compilation;
+using System.Windows.Forms;
+
+namespace BringYourOwnBuilder
+{
+    public class GoHaveFun : BuildProvider
+    {
+        public override void GenerateCode(AssemblyBuilder assemblyBuilder)
+        {
+            MessageBox.Show("Bruins Stink. Leafs for the Cup.");
+        }
+
+    }
+}
